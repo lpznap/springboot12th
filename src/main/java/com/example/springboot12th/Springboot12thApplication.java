433c00d06d4ec1c624879bf7152987a0c34d0245
ZNAPEYE <lpdeathheart@gmail.com>
2019-11-02 15:20:10 +0700

    New project

---
+++ src/main/java/com/example/springboot12th/Springboot12thApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot12th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot12thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot12thApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/springboot12th/controller/EmployeeController.java
@@ -0,0 +1,6 @@
+package com.example.springboot12th.controller;
+
+
+
+public class EmployeeController {
+}
